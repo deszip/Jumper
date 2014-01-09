7a3fc4223b0711daca11d4c02321031c5fd8fd8e
Deszip <igor.asharenkov@alterplay.com>
2014-01-09 16:08:38 +0200

    Initial Commit

---
+++ Jumper/Jumper.h
@@ -0,0 +1,13 @@
+//
+//  Jumper.h
+//  Jumper
+//
+//  Created by Deszip on 09.01.14.
+//  Copyright (c) 2014 Alterplay. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface Jumper : NSObject
+
+@end
